00c2c2373a521644613117a44e205b21be57a97d
Yusuf Irwandi <yusuf.irwandi@olx.co.id>
2018-07-30 13:36:23 +0700

    BruteTool

---
+++ src/BruteTool.php
@@ -0,0 +1,15 @@
+<?php
+namespace PackageA;
+
+class BruteTool
+{
+    public function powerBy2($int)
+    {
+        return pow($int, 2);
+    }
+
+    public function squareRoot($int)
+    {
+        return sqrt($int);
+    }
+}
